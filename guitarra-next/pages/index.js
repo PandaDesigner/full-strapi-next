952863875d997d9193bce8db0e97d667a3fc51cf
Pedro Fernandez <pand4designers@gmail.com>
2022-10-22 19:11:34 -0500

    main

--- guitarra-next/pages/index.js
+++ guitarra-next/pages/index.js
@@ -1,17 +1,3 @@
-/**
-    * @description      : 
-    * @author           : 
-    * @group            : 
-    * @created          : 22/10/2022 - 19:06:56
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 22/10/2022
-    * - Author          : 
-    * - Modification    : 
-**/
-import Head from 'next/head'
-import Image from 'next/image'
 import {Typography} from '@mui/material'
 
 export default function Home() {
