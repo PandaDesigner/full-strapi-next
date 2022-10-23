e1020bb136a8706d40898c170b721258083c095d
Pedro Fernandez <pand4designers@gmail.com>
2022-10-22 20:30:16 -0500

    master

--- guitarra-next/pages/index.js
+++ guitarra-next/pages/index.js
@@ -1,9 +1,21 @@
-import {Typography} from '@mui/material'
+import Head from 'next/head'
+import {Typography, Container} from '@mui/material'
+
+
 
 export default function Home() {
+
+
   return (
-    <Typography variant="h2" component="h2">
-      Desde Index con material UI
-    </Typography>
+    <>
+    <Head>
+    </Head>
+    <Container maxWidth="xl">
+      <Typography variant="h2" component="h2" >
+        Desde Index con material UI
+      </Typography>
+      
+    </Container>
+    </>
   )
 }
