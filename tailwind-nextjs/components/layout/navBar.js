7a6abff130761a006e911f248aedb9fb2c8a0fcb
Pedro Fernandez <pand4designers@gmail.com>
2022-10-30 19:53:20 -0500

    master¡

--- tailwind-nextjs/components/layout/navBar.js
+++ tailwind-nextjs/components/layout/navBar.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import Logo from '../../public/img/logo.svg'
 import BtnMenu from './btnMenu'
+import styles from '../../styles/header.module.css'
 
 
 const navigation = [
@@ -15,15 +16,15 @@ const navigation = [
 const NavBar = () => {
 
   return (
-    <>
-    <nav className="py-[5rem] bg-[url('https://res.cloudinary.com/dabysu4yd/image/upload/v1666402016/header_2a3d58d564.jpg?updated_at=2022-10-22T01:26:56.884Z')] bg-cover bg-center">
-    <div className='container flex items-center mx-auto flex-grow justify-between flex-col md:flex-row'>
+    <header className={styles.header}>
+    <nav className={`py-[5rem] bg-[url('https://res.cloudinary.com/dabysu4yd/image/upload/v1666402016/header_2a3d58d564.jpg?updated_at=2022-10-22T01:26:56.884Z')] bg-cover bg-center`}>
+    <div className={`container flex items-center mx-auto flex-grow justify-between flex-col md:flex-row `}>
     <Image src={Logo} className='md:w-1/2 md:flex-1 md:mb-4' width={250} height={80} alt='Logo' />
     <BtnMenu navigation={navigation} className='md:flex-auto'/>
     </div>
     </nav>
 
-    </>
+    </header>
   )
 }
 
