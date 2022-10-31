118ccfddd6e0845649cc1c8a13f305c5635c2eed
Pedro Fernandez <pand4designers@gmail.com>
2022-10-30 19:54:34 -0500

    imagen de fondo

--- tailwind-nextjs/components/layout/navBar.js
+++ tailwind-nextjs/components/layout/navBar.js
@@ -17,7 +17,7 @@ const NavBar = () => {
 
   return (
     <header className={styles.header}>
-    <nav className={`py-[5rem] bg-[url('https://res.cloudinary.com/dabysu4yd/image/upload/v1666402016/header_2a3d58d564.jpg?updated_at=2022-10-22T01:26:56.884Z')] bg-cover bg-center`}>
+    <nav className={`py-[5rem]  bg-[url('https://res.cloudinary.com/dabysu4yd/image/upload/v1666402016/header_2a3d58d564.jpg?updated_at=2022-10-22T01:26:56.884Z')] bg-cover bg-center`}>
     <div className={`container flex items-center mx-auto flex-grow justify-between flex-col md:flex-row `}>
     <Image src={Logo} className='md:w-1/2 md:flex-1 md:mb-4' width={250} height={80} alt='Logo' />
     <BtnMenu navigation={navigation} className='md:flex-auto'/>
