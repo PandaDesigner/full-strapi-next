a453aba8c63b7d44c0a1f52bf2a483862f39819e
Pedro Fernandez <pand4designers@gmail.com>
2022-10-31 23:29:01 -0500

    tienda de peliculas

--- tailwind-nextjs/pages/virtualShop.js
+++ tailwind-nextjs/pages/virtualShop.js
@@ -1,6 +1,5 @@
-import React from 'react'
 import Layout from '../components/layout'
-import ListadoGuitarra from '../components/listado-guitarra'
+import Guitarra from '../components/guitarra'
 
 
 const page = {
@@ -20,17 +19,41 @@ const VirtualShop = ({guitarra}) => {
     >
     <main className='container p-[2rem]'>
       <h1 className="text-3xl uppercase font-bold text-center text-yellow-500 mb-5">Nuestra Colección</h1>
-
-      <ListadoGuitarra/>
-
+      <div tabindex="0" class="focus:outline-none">
+      <div class="mx-auto container py-8">
+        <div class="flex flex-wrap items-center lg:justify-between justify-center">
+      {guitarra.map(guitarra =>(
+        <Guitarra
+        key={guitarra.id}
+        guitarra={guitarra.attributes}
+        />
+      )
+)}
+      </div>
+    </div>
+  </div>
     </main>
     </Layout>
   )
 }
 
 export default VirtualShop;
-
+/* 
 export async function getStaticProps(){
+
+  const res = await fetch(`${process.env.API_URL}/guitarras/?populate=*`)
+  const { data:guitarra } = await res.json()
+  return {
+     props:{
+      guitarra
+     }
+  }
+
+}
+ */
+
+
+export async function getServerSideProps(){
 
   const res = await fetch(`${process.env.API_URL}/guitarras/?populate=*`)
   const { data:guitarra } = await res.json()
