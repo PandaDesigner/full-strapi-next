e7aa7b11995fc44d4c2bfce8ccd9064c91438a21
Pedro Fernandez <pand4designers@gmail.com>
2022-11-02 19:41:55 -0500

    master

--- tailwind-nextjs/components/guitarra.js
+++ tailwind-nextjs/components/guitarra.js
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Link from "next/link"
 
 
 const Guitarra = ({guitarra}) => {
@@ -8,15 +8,15 @@ const { nombre, descriccion, imagen, precio, url } = guitarra,
 
     return (<>
            {/*  <!-- Card 1 --> */}
-
-                    <div tabindex="0" class="focus:outline-none mx-2 w-72 xl:mb-0 mb-8">
+                    <Link href={url}>
+                    <div tabindex="0" className="focus:outline-none mx-2 w-72 xl:mb-0 mb-8 cursor-pointer">
                         <div>
-                            <img alt="person capturing an image" src="https://cdn.tuk.dev/assets/templates/classified/Bitmap (1).png" tabindex="0" class="focus:outline-none w-full h-44" />
+                            <img alt="person capturing an image" src={imagen.data.attributes.url} tabindex="0" className="focus:outline-none h-[25] bg-cover bg-center" />
                         </div>
-                        <div class="bg-white">
-                            <div class="flex items-center justify-between px-4 pt-4">
+                        <div className="bg-white">
+                            <div className="flex items-center justify-between px-4 pt-4">
                                 <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" tabindex="0" class="focus:outline-none" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" tabindex="0" className="focus:outline-none" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                         <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2"></path>
                                     </svg>
@@ -27,13 +27,13 @@ const { nombre, descriccion, imagen, precio, url } = guitarra,
                             </div>
                             <div class="p-4">
                                 <div class="flex items-center">
-                                    <h2 tabindex="0" class="focus:outline-none text-lg font-semibold">{ nombre }</h2>
-                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">4 days ago</p>
+                                    <h2 tabindex="0" className="focus:outline-none text-lg font-semibold">{ nombre }</h2>
+                                    <p tabindex="0" className="focus:outline-none text-xs text-gray-600 pl-5">4 days ago</p>
                                 </div>
-                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2">{ acortarText(descriccion) }</p>
+                                <p tabindex="0" className="focus:outline-none text-xs text-gray-600 mt-2">{ acortarText(descriccion) }</p>
                                 
-                                <div class="flex items-center justify-between py-4">
-                                    <h2 tabindex="0" class="focus:outline-none text-indigo-700 text-xs font-semibold">
+                                <div className="flex items-center justify-between py-4">
+                                    <h2 tabindex="0" className="focus:outline-none text-indigo-700 text-xs font-semibold">
                                         {`ir a ${nombre}`}
                                     </h2>
                                 </div>
@@ -41,6 +41,7 @@ const { nombre, descriccion, imagen, precio, url } = guitarra,
                         </div>
                     </div>
                     {/* <!-- Card 1 Ends --> */}
+                    </Link>
                     </>
   )
 }
